c9c6695014e5572ed85741a2416e6b74f08c9498
AverageChapstick <73136332+AverageChapstick@users.noreply.github.com>
2022-11-26 11:27:46 -0800

    More prototyping

--- Scenes/EarthGravity.gd
+++ Scenes/EarthGravity.gd
@@ -1,1 +1,9 @@
 extends Area2D
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left_click"):
+		gravity = -500
+	elif Input.is_action_pressed("right_click"):
+		gravity = 500
+	else:
+		gravity = 100
