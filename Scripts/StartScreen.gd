495efe6885978eef1cb005c4adcaeceb319f3647
AverageChapstick <73136332+AverageChapstick@users.noreply.github.com>
2022-11-27 09:30:31 -0800

    Menu Sounds

--- Scripts/StartScreen.gd
+++ Scripts/StartScreen.gd
@@ -6,12 +6,24 @@ signal open_how_to_play
 
 func _on_StartGame_pressed():
 	emit_signal("start_game")
-
+	$Start.playing = true
+	
 func _on_Settings_pressed():
 	emit_signal("open_settings")
+	$Start.playing = true
 
 func _on_HowToPlay_pressed():
 	emit_signal("open_how_to_play")
-
+	$Start.playing = true
+	
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _on_StartGame_mouse_entered():
+	$Hover.playing = true
+
+func _on_Settings_mouse_entered():
+	$Hover.playing = true
+
+func _on_HowToPlay_mouse_entered():
+	$Hover.playing = true
