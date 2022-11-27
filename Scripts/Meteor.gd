7ab0e011dbde29f168fdc13c18713a859833a09c
AverageChapstick <73136332+AverageChapstick@users.noreply.github.com>
2022-11-27 09:39:26 -0800

    Explosions

--- Scripts/Meteor.gd
+++ Scripts/Meteor.gd
@@ -4,17 +4,23 @@ func _on_Meteor_body_entered(body):
 	if body.is_in_group("station"):
 		emit_signal("station_hit")
 		get_node("/root/Node").station_hit()
-		queue_free()
+		despawn()
 	elif body.is_in_group("earth"):
 		get_node("/root/Node").earth_hit()
 		queue_free()
 	else:
 		if randi() % 2:
 			get_node("/root/Node").get_push_fuel()
-			queue_free()
+			despawn()
 
 func _process(_delta):
 	if not $Timer.time_left:
 		if not position.x == clamp(position.x, -10, 250) or not position.y == clamp(position.y, -10, 145):
 			get_node("/root/Node").get_pull_fuel()
-			queue_free()
+			despawn()
+
+func despawn():
+	$Particles2D.emitting = true
+	$Explosion.playing = true
+	yield(get_tree().create_timer(1.0), "timeout")
+	queue_free()
